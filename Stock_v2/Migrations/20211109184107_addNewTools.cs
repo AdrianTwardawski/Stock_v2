f827b493342f24dbb0e282c7ee65dcaabcf975a3
adriantwardy <adriantwardawski@gmail.com>
2021-11-10 19:49:59 +0100

    Add new files

---
+++ Stock_v2/Migrations/20211109184107_addNewTools.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock_v2.Migrations
+{
+    public partial class addNewTools : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
