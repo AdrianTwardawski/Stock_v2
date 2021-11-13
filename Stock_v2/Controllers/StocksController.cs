1e67ecb72807671e94d383529bf43869dc6a4ef7
adriantwardy <adriantwardawski@gmail.com>
2021-11-13 18:00:19 +0100

    CreateStockScrapper

---
+++ Stock_v2/Controllers/StocksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stock_v2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_v2.Controllers
+{
+    public class StocksController : Controller
+    {
+        public IActionResult Index()
+        {
+            var stockScraper = new StockScrapper();
+            var stocks = stockScraper.GetStocks();
+            return View(stocks);
+        }
+    }
+}
---
+++ Stock_v2/Models/ViewModels/StocksViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock_v2.Models.ViewModels
+{
+    public class StocksViewModel
+    {
+        public string Walor { get; set; }
+        public float Kurs { get; set; }       
+        public float Zmiana { get; set; }
+    }
+}
